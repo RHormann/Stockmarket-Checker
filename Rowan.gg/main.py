8ea1212118087fdf23cb7e0b2848491def1735cd
RHormann <89350860+RHormann@users.noreply.github.com>
2022-11-07 20:45:12 +0000

    updated main
    small update to put the api key in another file

--- Rowan.gg/main.py
+++ Rowan.gg/main.py
@@ -1,24 +1,24 @@
 import requests
 import champIds
+import key
 
 #Get the summoner name and using that to get the encrypted ID as we need that to do most things
 summonerName = input("Enter your summoner name: ")
-api_key = "RGAPI-774b5cf4-4fea-44fd-b04f-08f30ede74d3"
-summonerInfo = f"https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={api_key}"
+summonerInfo = f"https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={key.key}"
 SIresponse = requests.get(summonerInfo)
 summonerInfoMap = SIresponse.json()
 icon = summonerInfoMap["profileIconId"]
-level= summonerInfoMap["summonerLevel"]
+level = summonerInfoMap["summonerLevel"]
 encryptedId = summonerInfoMap["id"]
 puuId = summonerInfoMap["puuid"]
 
 #Champion Mastery
-masteryStats = f"https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedId}/top?count=3&api_key={api_key}"
+masteryStats = f"https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedId}/top?count=3&api_key={key.key}"
 MSresponse = requests.get(masteryStats)
 masteryStatsMap = MSresponse.json()
 
 #Ranked stats of the player
-rankedStats = f"https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedId}?api_key={api_key}"
+rankedStats = f"https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/{encryptedId}?api_key={key.key}"
 
 
 #Store all the above data in a class for easy access
@@ -40,12 +40,13 @@ class summonerData:
     def getRankedStats(self):
         pass
     
+    #i cant figure out how to convert UNIX time to a readble time so have to come back to this, date time did not seem to work
     def getTopChamps(self, masteryStatsMap):
         for i in masteryStatsMap:
             champid = i["championId"]
             champ = champIds.getChampionId(champid)
             points = str(i["championPoints"])
-            lastPlayed = str(i["lastPlayTime"])
+            lastPlayed = i["lastPlayTime"]
             print(f"    {champ}: {points} Mastery Points")
             print(f"    Last played: {lastPlayed}\n")
     
@@ -53,9 +54,24 @@ class summonerData:
         pass
 
 User = summonerData(icon, level, summonerName)
-print("")
-print(f"{User.getName()}:")
-print(f"    Icon --> {User.getIcon()}")
-print(f"    Summoner Level --> {User.getLevel()}")
-print(f"\nChampion Mastery:")
-print(f"{User.getTopChamps(masteryStatsMap)}")
+#the way the information will be displayed in the terminal, can choose to only display certain sections
+class printFormat:
+    def __init__(self) -> None:
+        pass
+
+    def getBasicInfo(self):
+        print(f"\n{User.getName()}:")
+        print(f"    Icon --> {User.getIcon()}")
+        print(f"    Summoner Level --> {User.getLevel()}")
+    
+    def getChampionMasteryInfo(self):
+        print(f"\nChampion Mastery:")
+        print(f"{User.getTopChamps(masteryStatsMap)}")
+
+    def getAll(self):
+        self.getBasicInfo()
+        self.getChampionMasteryInfo()
+
+display = printFormat()
+
+display.getAll()
